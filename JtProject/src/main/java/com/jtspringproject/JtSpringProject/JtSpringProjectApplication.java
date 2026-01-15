95b207778c9dd08cda28ef680d9c25908b1c8415
AssianAk <107809552+AssianAk@users.noreply.github.com>
2026-01-16 00:30:14 +0530

    Update JtSpringProjectApplication.java

--- JtProject/src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
+++ JtProject/src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
@@ -2,13 +2,19 @@ package com.jtspringproject.JtSpringProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+@SpringBootApplication(
+    exclude = {
+        HibernateJpaAutoConfiguration.class,
+        JpaRepositoriesAutoConfiguration.class
+    }
+)
 public class JtSpringProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JtSpringProjectApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JtSpringProjectApplication.class, args);
+    }
 }
+
